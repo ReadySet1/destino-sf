a5fe377ffd9666b38a630e7204d46337d422f32b
Fernando Cardenas <fsanchezcln@gmail.com>
2025-05-09 10:36:11 -0600

    ✅ Updated variables

--- src/components/Maps/GoogleMaps.tsx
+++ src/components/Maps/GoogleMaps.tsx
@@ -1,9 +1,6 @@
 'use client';
 
 import { Loader } from '@googlemaps/js-api-loader';
-import { laserwave } from '@react-email/components';
-import { init } from 'next/dist/compiled/webpack/webpack';
-import { version } from 'os';
 import { useEffect, useRef } from 'react';
 
 function GoogleMaps() {
@@ -11,39 +8,43 @@ function GoogleMaps() {
 
   useEffect(() => {
     const initMap = async () => {
-      const loader = new Loader({
-        apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '',
-        version: 'quarterly',
-        libraries: ['places'],
-      });
+      try {
+        const loader = new Loader({
+          apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '',
+          version: 'quarterly',
+          libraries: ['places'],
+        });
 
-      const { Map } = await loader.importLibrary('maps');
+        const { Map } = await loader.importLibrary('maps');
 
-      // Destino Location
-      const location = {
-        lat: 37.7266587,
-        lng: -122.368223217,
-      };
-
-      const options: google.maps.MapOptions = {
-        center: location,
-        zoom: 15,
-        mapId: 'map',
-      };
-
-      const map = new Map(mapRef.current as HTMLElement, options);
-
-      const { AdvancedMarkerElement } = (await loader.importLibrary(
-        'marker'
-      )) as google.maps.MarkerLibrary;
-
-      new AdvancedMarkerElement({
-        position: {
+        // Destino Location
+        const location = {
           lat: 37.7266587,
           lng: -122.368223217,
-        },
-        map: map,
-      });
+        };
+
+        const options: google.maps.MapOptions = {
+          center: location,
+          zoom: 15,
+          mapId: 'map',
+        };
+
+        const map = new Map(mapRef.current as HTMLElement, options);
+
+        const { AdvancedMarkerElement } = (await loader.importLibrary(
+          'marker'
+        )) as google.maps.MarkerLibrary;
+
+        new AdvancedMarkerElement({
+          position: {
+            lat: 37.7266587,
+            lng: -122.368223217,
+          },
+          map: map,
+        });
+      } catch (error) {
+        console.error('Error al cargar o inicializar el mapa:', error);
+      }
     };
 
     initMap();
@@ -51,4 +52,5 @@ function GoogleMaps() {
 
   return <div ref={mapRef} className="w-full h-screen" />;
 }
+
 export default GoogleMaps;
