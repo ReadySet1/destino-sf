377c88d5fbbfcf70ae439e481ca3c50625d2b2a3
Emmanuel Alanis <ealanisln@me.com>
2025-07-23 16:04:26 -0600

    Add Store components index file and improve GitHub Actions debugging

---
+++ src/components/store/index.ts
@@ -0,0 +1,18 @@
+// Export all Store components
+export { CartItemList } from './CartItemList';
+export { CartSummary } from './CartSummary';
+export { EmptyCart } from './EmptyCart';
+export { FulfillmentSelector } from './FulfillmentSelector';
+export { PaymentMethodSelector } from './PaymentMethodSelector';
+export { CartItemRow } from './CartItemRow';
+export { AddToCartButton } from './AddToCartButton';
+export { AddressForm } from './AddressForm';
+export { AccountProfile } from './AccountProfile';
+export { CheckoutForm } from './CheckoutForm';
+export { CheckoutSummary } from './CheckoutSummary';
+export { NavBar } from './NavBar';
+export { OrderHistory } from './OrderHistory';
+export { ProductCard } from './ProductCard';
+export { ProductDetail } from './ProductDetail';
+export { CategoryTabs } from './CategoryTabs';
+export { QuantityStepper } from './QuantityStepper'; 
