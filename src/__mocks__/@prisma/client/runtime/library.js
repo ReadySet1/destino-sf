1b02507abed17111284b0bdd8ac849848fb63fbf
Emmanuel Alanis <ealanisln@me.com>
2025-10-30 15:39:12 -0600

    test(mocks): add Decimal class to Prisma runtime library mock
    - Add mock implementation of Prisma Decimal class with basic math operations
    - Fixes unit test failures where test-utils.ts uses new Decimal()
    - Ensures unit tests and component tests both pass
    
    This completes the test infrastructure fixes for the spotlight picks API change.

--- src/__mocks__/@prisma/client/runtime/library.js
+++ src/__mocks__/@prisma/client/runtime/library.js
@@ -1,4 +1,50 @@
 // Manual mock for @prisma/client/runtime/library
+class Decimal {
+  constructor(value) {
+    this.value = typeof value === 'string' ? parseFloat(value) : value;
+  }
+
+  toString() {
+    return this.value.toString();
+  }
+
+  toNumber() {
+    return this.value;
+  }
+
+  toFixed(decimals) {
+    return this.value.toFixed(decimals);
+  }
+
+  equals(other) {
+    return this.value === (other.value || other);
+  }
+
+  lessThan(other) {
+    return this.value < (other.value || other);
+  }
+
+  greaterThan(other) {
+    return this.value > (other.value || other);
+  }
+
+  add(other) {
+    return new Decimal(this.value + (other.value || other));
+  }
+
+  sub(other) {
+    return new Decimal(this.value - (other.value || other));
+  }
+
+  mul(other) {
+    return new Decimal(this.value * (other.value || other));
+  }
+
+  div(other) {
+    return new Decimal(this.value / (other.value || other));
+  }
+}
+
 module.exports = {
   PrismaClientKnownRequestError: class extends Error {
     constructor(message, { code, clientVersion }) {
@@ -8,4 +54,5 @@ module.exports = {
       this.name = 'PrismaClientKnownRequestError';
     }
   },
+  Decimal,
 };
